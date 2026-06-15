9454bc21126463b4d5819108ead160e6ce742957
Archana <archana23961@gmail.com>
2026-06-15 12:45:26 +0530

    Implemented JWT authentication and role based access control

--- employee-service/src/controllers/employeeController.js
+++ employee-service/src/controllers/employeeController.js
@@ -1,20 +1,78 @@
 const db = require("../config/db");
 const { getChannel } = require("../config/rabbitmq");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+// Register Employee
 exports.registerEmployee = async (req, res) => {
   try {
-    const {
-      employeeId,
-      name,
-      email,
-      department
-    } = req.body;
+   const {
+  employeeId,
+  name,
+  email,
+  department,
+  password,
+  role
+} = req.body;
 
-    // Validation
-    if (!employeeId || !name || !email || !department) {
+
+    // Required fields validation
+   if (
+  !employeeId ||
+  !name ||
+  !email ||
+  !department ||
+  !password
+) {
       return res.status(400).json({
         success: false,
         message: "All fields are required"
+      });
+    }
+
+    // Employee ID validation
+    const employeeIdRegex = /^EMP\d+$/;
+
+    if (!employeeIdRegex.test(employeeId)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid employee ID format"
+      });
+    }
+
+    // Name validation
+    const nameRegex = /^[A-Za-z ]+$/;
+
+    if (!nameRegex.test(name)) {
+      return res.status(400).json({
+        success: false,
+        message: "Name should contain only letters"
+      });
+    }
+
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email format"
+      });
+    }
+
+    // Department validation
+    const validDepartments = [
+      "Engineering",
+      "HR",
+      "IT",
+      "Finance",
+      "Marketing"
+    ];
+
+    if (!validDepartments.includes(department)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid department"
       });
     }
 
@@ -35,15 +93,32 @@ exports.registerEmployee = async (req, res) => {
       });
     }
 
-    // Save employee to MySQL
+    const hashedPassword = await bcrypt.hash(
+  password,
+  10
+);
+    // Save employee
     await db.query(
       `
-      INSERT INTO employees
-      (employee_id, name, email, department)
-      VALUES (?, ?, ?, ?)
+     INSERT INTO employees
+(
+  employee_id,
+  name,
+  email,
+  department,
+  password,
+  role
+)
+VALUES (?, ?, ?, ?, ?, ?)
       `,
-      [employeeId, name, email, department]
-    );
+[
+  employeeId,
+  name,
+  email,
+  department,
+  hashedPassword,
+  role || "employee"
+]    );
 
     // Publish RabbitMQ event
     const channel = getChannel();
@@ -80,6 +155,78 @@ exports.registerEmployee = async (req, res) => {
     });
   }
 };
+// Login Employee
+exports.loginEmployee = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validation
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required"
+      });
+    }
+
+    // Find employee
+    const [employees] = await db.query(
+      `
+      SELECT *
+      FROM employees
+      WHERE email = ?
+      `,
+      [email]
+    );
+
+    if (employees.length === 0) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+    }
+
+    const employee = employees[0];
+
+    // Compare password
+    const isMatch = await bcrypt.compare(
+      password,
+      employee.password
+    );
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+    }
+
+    // Generate JWT
+    const token = jwt.sign(
+      {
+        employeeId: employee.employee_id,
+        role: employee.role
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d"
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      token
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error"
+    });
+  }
+};
+// Get Employee By ID
 exports.getEmployee = async (req, res) => {
   try {
     const { employeeId } = req.params;
@@ -114,6 +261,8 @@ exports.getEmployee = async (req, res) => {
     });
   }
 };
+
+// Get All Employees
 exports.getAllEmployees = async (req, res) => {
   try {
     const [employees] = await db.query(
@@ -137,6 +286,8 @@ exports.getAllEmployees = async (req, res) => {
     });
   }
 };
+
+// Update Employee
 exports.updateEmployee = async (req, res) => {
   try {
     const { employeeId } = req.params;
@@ -147,11 +298,47 @@ exports.updateEmployee = async (req, res) => {
       department
     } = req.body;
 
-    // Validation
+    // Required fields validation
     if (!name || !email || !department) {
       return res.status(400).json({
         success: false,
         message: "All fields are required"
+      });
+    }
+
+    // Name validation
+    const nameRegex = /^[A-Za-z ]+$/;
+
+    if (!nameRegex.test(name)) {
+      return res.status(400).json({
+        success: false,
+        message: "Name should contain only letters"
+      });
+    }
+
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email format"
+      });
+    }
+
+    // Department validation
+    const validDepartments = [
+      "Engineering",
+      "HR",
+      "IT",
+      "Finance",
+      "Marketing"
+    ];
+
+    if (!validDepartments.includes(department)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid department"
       });
     }
 
@@ -184,7 +371,26 @@ exports.updateEmployee = async (req, res) => {
       `,
       [name, email, department, employeeId]
     );
+// Publish RabbitMQ event
+const channel = getChannel();
 
+if (channel) {
+  await channel.assertQueue("employee.updated");
+
+  channel.sendToQueue(
+    "employee.updated",
+    Buffer.from(
+      JSON.stringify({
+        employeeId
+      })
+    )
+  );
+
+  console.log(
+    "Published employee.updated:",
+    employeeId
+  );
+}
     res.status(200).json({
       success: true,
       message: "Employee updated successfully"
@@ -199,6 +405,8 @@ exports.updateEmployee = async (req, res) => {
     });
   }
 };
+
+// Delete Employee
 exports.deleteEmployee = async (req, res) => {
   try {
     const { employeeId } = req.params;
@@ -228,6 +436,27 @@ exports.deleteEmployee = async (req, res) => {
       `,
       [employeeId]
     );
+
+    // Publish RabbitMQ event
+    const channel = getChannel();
+
+    if (channel) {
+      await channel.assertQueue("employee.deleted");
+
+      channel.sendToQueue(
+        "employee.deleted",
+        Buffer.from(
+          JSON.stringify({
+            employeeId
+          })
+        )
+      );
+
+      console.log(
+        "Published employee.deleted:",
+        employeeId
+      );
+    }
 
     res.status(200).json({
       success: true,
